d2f852f82d8405da3d0646035b15f1ee43ca0673
joydebn <34549773+joydebn@users.noreply.github.com>
2026-03-22 15:28:22 +0530

    Update keyvault.tf

--- keyvault.tf
+++ keyvault.tf
@@ -3,8 +3,8 @@ data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "dev" {
   name                = "kv-dev-aks01"
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "standard"
 
