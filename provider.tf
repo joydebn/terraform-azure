cf5a61d79e799c2dd792303a451abb5728ba7551
joydebn <34549773+joydebn@users.noreply.github.com>
2026-03-22 15:32:09 +0530

    Enhance azurerm provider with key vault features
    Add key vault features to azurerm provider configuration

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
 terraform {
   required_version = ">= 1.5.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -17,5 +18,10 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy      = true
+      recover_soft_deleted_key_vaults   = false
+    }
+  }
 }
