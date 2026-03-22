f30591980d916e966ea6104ef6a5b49a90d3c477
joydebn <34549773+joydebn@users.noreply.github.com>
2026-03-22 13:48:04 +0530

    Update keyvault.tf

--- keyvault.tf
+++ keyvault.tf
@@ -11,7 +11,7 @@ resource "azurerm_key_vault" "dev" {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
-    secret_permissions = ["get", "list"]
+    secret_permissions = ["Get", "List"]
   }
 }
 
