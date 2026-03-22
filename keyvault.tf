0cae5c38b98879758eba2bc476833baad9bc5f94
joydebn <34549773+joydebn@users.noreply.github.com>
2026-03-22 13:16:19 +0530

    Update keyvault.tf
    update keyvault

--- keyvault.tf
+++ keyvault.tf
@@ -1,21 +1,17 @@
-data "azurerm_client_config" "current" {}
-
-resource "azurerm_key_vault" "kv" {
-  name                = "my-keyvault"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "standard"
+data "azurerm_key_vault" "dev" {
+  name                = "kv-dev"
+  resource_group_name = "rg-dev"
 }
 
 data "azurerm_key_vault_secret" "mysql_password" {
-  name         = "mysql-admin-password"
-  key_vault_id = azurerm_key_vault.kv.id
+  name         = "mysql-password"
+  key_vault_id = data.azurerm_key_vault.dev.id
 }
 
-variable "mysql_admin_password" {
-  description = "MySQL admin password"
-  type        = string
-  sensitive   = true
-  default     = data.azurerm_key_vault_secret.mysql_password.value
+resource "azurerm_mysql_server" "example" {
+  name                         = var.mysql_server_name
+  location                     = var.location
+  resource_group_name          = var.resource_group_name
+  administrator_login          = var.mysql_admin_username
+  administrator_login_password = data.azurerm_key_vault_secret.mysql_password.value
 }
