cd77a482f86c3f3083a59dd29dcee9056467f3d8
joydebn <34549773+joydebn@users.noreply.github.com>
2026-03-22 14:18:15 +0530

    Update keyvault.tf

--- keyvault.tf
+++ keyvault.tf
@@ -1,7 +1,7 @@
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "dev" {
-  name                = "kv-dev-aks01"
+  name                = "kv-dev-aks01"   # must be globally unique
   location            = var.location
   resource_group_name = var.resource_group_name
   tenant_id           = data.azurerm_client_config.current.tenant_id
@@ -11,17 +11,13 @@ resource "azurerm_key_vault" "dev" {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
+    # Allow Terraform identity to read and set secrets
     secret_permissions = ["Get", "List", "Set"]
   }
 }
 
 resource "azurerm_key_vault_secret" "mysql_password" {
   name         = "mysql-password"
-  value        = "SuperSecret123!" # or use Terraform variable
-  key_vault_id = azurerm_key_vault.dev.id
-}
-
-data "azurerm_key_vault_secret" "mysql_password" {
-  name         = "mysql-password"
+  value        = var.mysql_admin_password   # safer than hardcoding
   key_vault_id = azurerm_key_vault.dev.id
 }
