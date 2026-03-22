bd97e8c03cb5822eff5b8e87124cb56e309f0101
joydebn <34549773+joydebn@users.noreply.github.com>
2026-03-22 12:52:34 +0530

    Update backend.tf
    change storage account name

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
-    storage_account_name = "tfstateaccount"
+    storage_account_name = "tfstateaccount0005"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
